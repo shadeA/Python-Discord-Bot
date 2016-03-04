edfceaf93ded6f99d410aa52bcd5af0e738e8aac
shadeA <daniel.random@outlook.com>
2016-03-03 21:17:28 -0700

    Cleaning Up
    Cleaned up and prepared some code for the Infrastructure update that is
    coming soon. Also improved readability

--- audio.py
+++ audio.py
@@ -2,36 +2,33 @@ import asyncio
 import youtube_dl
 import discord
 import logging
+import livestreamer
 
 logging.basicConfig(level=logging.INFO)
 
-async def play_audio(message, audio_type, client):
-    if client.player is not None and client.player.is_playing():
-        client.player.stop()
-        logging.info('Stopped {} from playing'.format(client.current))
+async def play_audio(message, bot):
+    _send = bot.send_message()
 
-    if audio_type == 'url':
-        if client.current is not None:
-            client.previous = client.current
+    if bot.player is not None and bot.player.is_playing():
+        bot.player.stop()
+        logging.info('Stopped {} from playing'.format(bot.current))
 
-        client.player = await client.voice.create_ytdl_player(message)
-        client.player.start()
-        client.current = client.player.title
-        await client.send_message(client.message.channel, "{userName} started playing {songName}".format(userName=client.message.author.name, songName=client.current))
+    if bot.current is not None: #If a previous song has been played
+        bot.previous = bot.current #Replace old previous song with the current song to prepare for the next song
+
+    if 'youtube' in message:
+        message = message.split()
+        try:
+            url = message[0]
+            name = message[1]
+        except IndexError:
+            _send(bot.message.author, audio_error)
+
+        bot.player = await bot.voice.create_ytdl_player(message)
+        bot.player.start()
+        bot.current = bot.player.title
+        await bot.send_message(bot.message.channel, "{userName} started playing {songName}".format(userName=bot.message.author.name, songName=bot.current))
             # I send client.current rather then client.player.title for consistency. Oddly enough, I never really learned if this is good or bad. Just seems right. Hopefully it is.
         return
-    elif audio_type == 'file':
 
-        if '.mp3' not in message:
-            logging.error('Path passed did not contain a .mp3 extension. Due to limitations in my skills, this is nessasary. Inform the user.')
-            await client.send_message(client.message.author, "{}, The song you reequested did not contain a .mp3 extension. Make sure this is in your file name.")
-            return
-        if client.current is not None:
-            client.previous = client.current
-
-        client.player = client.voice.create_ffmpeg_player("media/" + message)
-        client.player.start()
-        client.current = message.replace('.mp3', "", 1)
-        await client.send_message(client.message.channel, "{userName} started playing {songName}".format(userName=client.message.author.name, songName=client.current))
-        return
 
--- errors.py
+++ errors.py
@@ -1,4 +1,4 @@
-error_master = "{user}, there was an error handling your command,\n"
+steam_error = "{user}, there was an error handling your command,\n"
 "make sure you include {problem} and the"
 " correct spelling/formatting.\n"
 "An example for csgo updates is: \n"
@@ -7,12 +7,12 @@ error_master = "{user}, there was an error handling your command,\n"
 "A full list of game IDs can be found at:\n"
 "       https://steamdb.info/search/"
 
-error_unknown = "{user}, an unknown error has occured.\n {debug}" #Debug is an optional option that gets triggered if the user has enabled debug mode with .debug true. I have yet to implement this feature
+steam_errorUnknown = "{user}, an unknown error has occured.\n {debug}" #Debug is an optional option that gets triggered if the user has enabled debug mode with .debug true. I have yet to implement this feature
 
 
-errorText_Options = "the required options"
-errorText_ID = "the correct game/user ID"
-errorText_Param = "all of the needed parameters"
+steam_errorText_Options = "the required options"
+steam_errorText_ID = "the correct game/user ID"
+steam_errorText_Param = "all of the needed parameters"
 
 
 helpText = "{user}, the commands/options are as follow:\n"
@@ -20,3 +20,6 @@ helpText = "{user}, the commands/options are as follow:\n"
 "       -mode is a required parameter that precedes the action that you will be doing. Allowed actions are:\n"
 "               update: Gets reecent updatees/news for the game that corresponds to the game ID passeed with -id"
 "               user: Gets information about the user that matches the passed Steam64 ID through -id"
+
+
+audio_error = ""
